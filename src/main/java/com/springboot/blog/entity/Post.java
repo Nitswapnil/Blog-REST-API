48302fbd43986d6c857722dd820f9256c4b057b9
Nitin Kumar Maurya <nitinkumarmaurya2@gmail.com>
2024-04-22 22:47:08 +0530

    Creating JPA Entity Post

---
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,26 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+}
